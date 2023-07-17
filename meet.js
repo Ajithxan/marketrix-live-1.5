1b9d57936d329dce05bff82e7c09ce80651ca79e
Ajithxan <Ajithshanth.creativehub@gmail.com>
2023-07-17 13:32:23 +0530

    cc

--- meet.js
+++ meet.js
@@ -1,4 +1,4 @@
-const meetVersion = "1.1";
+const meetVersion = "1.2";
 const CDNlink = `https://cdn.jsdelivr.net/gh/Ajithxan/marketrix-live-${meetVersion}/`; //'http://localhost/creativehub/meet-source/'
 console.log(CDNlink);
 const startingTime = new Date().getTime();
